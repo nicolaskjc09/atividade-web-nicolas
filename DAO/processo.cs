bddc9a2d3e8cc0f97e4c033c8ee2757c10f63146
Nicolbol <160546200+nicolaskjc09@users.noreply.github.com>
2026-08-24 11:48:30 -0400

    NAO SEI O QUE FIZ

---
+++ DAO/processo.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace SindromeDe.DAO
+{
+    public class Processo
+    {
+        public int Id { get; set; }
+
+        public string Numero { get; set; } = string.Empty;
+
+        public DateOnly Data { get; set; }
+
+        public string Interessado { get; set; } = string.Empty;
+
+        public string Assunto { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public string Situacao { get; set; } = string.Empty;
+
+    }
+}
+
+/*CREATE TABLE processos (
+	id_pro INT NOT NULL AUTO_INCREMENT,
+    numero_pro VARCHAR(200) NOT NULL, 
+    data_pro DATE NOT NULL,
+    interessado_pro VARCHAR(200) NOT NULL, 
+    assunto_pro VARCHAR(300) NOT NULL,
+    descricao_pro TEXT NULL,
+    situacao_pro VARCHAR(50) NOT NULL DEFAULT "Aberto",
+    
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    PRIMARY KEY(id_pro)
+);
+
+*/
