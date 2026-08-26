4175de14997de5b38aba34a309647c88cdbfc2c4
Nicolbol <160546200+nicolaskjc09@users.noreply.github.com>
2026-08-26 08:17:12 -0400

    agr sim

---
+++ Model/Processo.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace AppWebExemplo.Model
+{
+    public class Processo
+    {
+        public int Id { get; set; }
+
+        public string Numero { get; set; } = string.Empty;
+
+        public DateOnly Data { get; set; }
+
+        public string Interessado { get; set; } = string.Empty;
+
+        public string Assunto { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public string Situacao { get; set; } = string.Empty;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using AppWebExemplo.Components;
+using AppWebExemplo.Configs;
+using AppWebExemplo.DAO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Configuração da Conexão com o Banco de Dados MYSQL
+builder.Services.AddScoped<Conexao>();
+builder.Services.AddScoped<ProcessoDAO>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
